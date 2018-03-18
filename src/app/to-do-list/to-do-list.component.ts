cf1a1c5de0d09f9830b7a31e41a32a2616a97663
Lukasz <lukmro76@gmail.com>
2018-03-18 23:48:18 +0100

    change awesome icons

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,15 +1,43 @@
+import { TodoService } from './to-do-list.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.less']
+  styleUrls: ['./to-do-list.component.less'],
+  providers: [TodoService ]
 })
 export class ToDoListComponent implements OnInit {
+  toDoListArray: any[];
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        const x = element.payload.toJSON();
+        x['$key'] = element.key;
+        this.toDoListArray.push(x);
+      });
+      this.toDoListArray.sort((a, b) => {
+        return a.isChecked - b.isChecked;
+      });
+    });
+  }
+
+  onAdd(itemTitle) {
+    this.todoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
+
+  alterCheck($key: string, isChecked) {
+    this.todoService.checkOrUnCheckTitle($key, !isChecked);
+  }
+
+  onDelete($key: string) {
+    this.todoService.removeTitle($key);
   }
 
 }
